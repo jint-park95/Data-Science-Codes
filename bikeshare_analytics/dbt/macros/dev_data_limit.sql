2dee3ced5a8a7c4b30148424ea7eaf2d44e2464f
jint.park95@gmail.com <jint.park95@gmail.com>
2022-07-04 02:04:49 +0000

    add macro for dev data limit

--- bikeshare_analytics/dbt/macros/dev_data_limit.sql
+++ bikeshare_analytics/dbt/macros/dev_data_limit.sql
@@ -3,8 +3,3 @@
 where {{ datetime_column }} >= date_sub( current_datetime(), interval {{ increment_value }} {{ increment }})
 {%- endif %}
 {%- endmacro -%}
-
-select
-    *
-from {{ ref('base__bike_location') }}
-{{ dev_data_limit(datetime_column = 'last_updated_ct', increment = 'day', increment_value = 2) }}
