aafe2f3ea63278df9c8157fff4bee92d1199bc6f
jint.park95@gmail.com <jint.park95@gmail.com>
2022-07-05 00:28:08 +0000

    remove columns

--- bikeshare_analytics/dbt/models/marts/dim_bike_trip.sql
+++ bikeshare_analytics/dbt/models/marts/dim_bike_trip.sql
@@ -8,7 +8,19 @@ with
 
 bike_location as (
 
-    select * from {{ ref('stg__bike_location_deduplicated') }}
+    select 
+
+        last_updated_ct,
+        bike_id,
+        lon,
+        lat,
+        bike_geo_loc,
+        time_diff_in_seconds,
+        distance_in_meter,
+        is_start_movement,
+        is_end_movement
+
+    from {{ ref('stg__bike_location_deduplicated') }}
 
 ),
 
@@ -28,7 +40,6 @@ bike_movement_order_joined as (
     select 
 
         bike_location.*,
-        mvmt_begin_rank,
         case
             when is_end_movement is true then null
             when is_start_movement is true then mvmt_begin_rank
@@ -36,21 +47,28 @@ bike_movement_order_joined as (
                 max(mvmt_begin_rank) over(
                     partition by bike_id 
                     order by last_updated_ct 
-                    rows between unbounded preceding and current_row
+                    rows between unbounded preceding and current row
                 )
             else null
         end as bike_trip_rank
 
     from bike_location
 
-    left join trip_start_order
+    left join movement_start_order
         using(last_updated_ct, bike_id)
+
+),
+
+group_bike_trip as (
+
+    select
+
+        *
+
+    from bike_movement_order_joined
 
 )
 
 
-select * from bike_movement_order_joined
-WHERE 
-  bike_id ="21ad824e-ee5e-4244-9227-e7cc6af85328"
-  AND last_updated_ct <= ("2022-07-02T18:40:26")
-order by last_updated_ct desc
+select * from group_bike_trip
+
